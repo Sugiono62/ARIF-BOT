d953fdda3a37c942cf2899c73dff43814a1147f1
Sugiono62 <108973863+Sugiono62@users.noreply.github.com>
2022-09-06 19:40:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,22 +3,22 @@
 //Dalam file _menunya.js
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx']  // isi jika perlu
-global.mods = ['6282xxxxxx']   // isi jika perlu
-global.prems = ['6282xxxxxx']  // isi jika perlu
+global.owner = ['6285865462352']  // isi jika perlu
+global.mods = ['6285865462352']   // isi jika perlu
+global.prems = ['6285865462352']  // isi jika perlu
 
 //Isi dengan nama dan nomor lu//
-global.nameowner = '⫹⫺ OWNER' // isi nama kalian bebas
-global.numberowner = '6282xxxxxx' // isi nomor kalian dengan awalan seperti di contoh 
-global.instagram = 'https://instagram.com/prm2.0' // isi Instagram kalian harus link
-global.github = 'https://github.com/BOTCAHX' // gausah di isi juga gpp bro
-global.dana = '6282xxxxxx' // isi dengan nomor dana yg kamu punya 
-global.pulsa = '6282xxxxxx' // isi dengan nomor pulsa hp kalian
-global.gopay = ''
+global.nameowner = '⫹⫺ Arif FD😈 // isi nama kalian bebas
+global.numberowner = '6285865462352' // isi nomor kalian dengan awalan seperti di contoh 
+global.instagram = 'https://instagram.com/dyandra_arif_faizaaa' // isi Instagram kalian harus link
+global.github = 'https://github.com/Sugiono62' // gausah di isi juga gpp bro
+global.dana = '6285865462352' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '62895808168700' // isi dengan nomor pulsa hp kalian
+global.gopay = '62895808168700'
 
 //Name Bot & Grouplink//
-global.namebot = 'R-txzyMd'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
+global.namebot = 'FAIZ BOZ'
+global.gc = '-'
 global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
@@ -27,10 +27,10 @@ global.fotonya1 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-stu
 global.fotonya2 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-studios?text=List%20&text2=Menu' 
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© FAIZZ'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ FAIZZ'
+global.wm3 = '© FAIZZ'
 global.wm4 = '© 2021/2022'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -42,7 +42,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot SESAD/FAIZZ BOZ'
 
 //Apikey Setting//
 global.APIs = { // API Prefix
